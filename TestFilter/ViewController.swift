b2fdd3ca86e4f85dd18bb0ef3fd913a64ad12b8b
domivarga <v.domi26@gmail.com>
2018-10-26 16:32:15 +0200

    Checks framework with checkout from the original, Brad Larson repository of GPUImage with Objective-C

--- TestFilter/ViewController.swift
+++ TestFilter/ViewController.swift
@@ -15,46 +15,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        imageView.image = createImageWithPipeline(image: UIImage(named: "test")!)
-
-//        createImage(image: UIImage(named: "test")!, completion: { img in
-//            self.imageView.image = img
-//        })
+        var image = createImageWithPipeline(image: UIImage(named: "test")!, filter: GPUImageBrightnessFilter())
+        image = createImageWithPipeline(image: image, filter: GPUImageContrastFilter())
+        imageView.image = createImageWithPipeline(image: image, filter: GPUImageSaturationFilter())         
     }
 
-    private func createImageWithPipeline(image: UIImage) -> UIImage {
-        let brightnessEffect = BrightnessAdjustment()
-        let contrastEffect = ContrastAdjustment()
-        let saturationEffect = SaturationAdjustment()
+    private func createImageWithPipeline(image: UIImage, filter: GPUImageFilter) -> UIImage {
+        let stillImageSource = GPUImagePicture(image: image)
 
-        brightnessEffect.brightness = 0
-        contrastEffect.contrast = 1
-        saturationEffect.saturation = 1
+        stillImageSource?.addTarget(filter)
+        filter.useNextFrameForImageCapture()
+        stillImageSource?.processImage()
 
-        let filteredImage = image.filterWithPipeline { input, output in
-            input --> brightnessEffect --> contrastEffect --> saturationEffect --> output
-        }
+        guard let img = stillImageSource?.imageFromCurrentFramebuffer(with: .up) else { return UIImage() }
 
-        return filteredImage
-    }
-
-    private func createImage(image: UIImage, completion: @escaping (UIImage) -> Void) {
-        let input = PictureInput(image: image)
-        let output = PictureOutput()
-        let brightnessEffect = BrightnessAdjustment()
-        let contrastEffect = ContrastAdjustment()
-        let saturationEffect = SaturationAdjustment()
-
-        brightnessEffect.brightness = 0
-        contrastEffect.contrast = 1
-        saturationEffect.saturation = 1
-
-        output.imageAvailableCallback = { image in
-            completion(image)
-        }
-
-        input --> brightnessEffect --> contrastEffect --> saturationEffect --> output
-        input.processImage(synchronously:true)
+        return img
     }
 }
 
